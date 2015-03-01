49280644e389b7fca797c2959933fc1949227420
mdkwock <mdk85@cornell.edu>
2015-03-01 10:47:04 -0500

    Auto-committing 'index.php'

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require './vendor/autoload.php';
 use GW2ledger\GuzzleWebScraper;
 use Monolog\Handler\AmqpHandler;
 $str = '{"name":"MONSTER ONLY Moa Unarmed Pet","type":"Weapon","level":0,"rarity":"Fine","vendor_value":0,"default_skin":3265,"game_types":["Activity","Dungeon","Pve","Wvw"],"flags":["NoSell","SoulbindOnAcquire","SoulBindOnUse"],"restrictions":[],"id":1,"icon":"https://render.guildwars2.com/file/4AECE5EA59CA057F4C53E1EDFE95E0E3E61DE37F/60980.png","details":{"type":"Staff","damage_type":"Physical","min_power":146,"max_power":165,"defense":0,"infusion_slots":[],"infix_upgrade":{"attributes":[]},"secondary_suffix_item_id":""}}';
-$json = json_decode($str);
+$json = json_decode($str,true);
 var_export($json);
-array( 'name' => 'MONSTER ONLY Moa Unarmed Pet', 'type' => 'Weapon', 'level' => 0, 'rarity' => 'Fine', 'vendor_value' => 0, 'default_skin' => 3265, 'game_types' => array ( 0 => 'Activity', 1 => 'Dungeon', 2 => 'Pve', 3 => 'Wvw', ), 'flags' => array ( 0 => 'NoSell', 1 => 'SoulbindOnAcquire', 2 => 'SoulBindOnUse', ), 'restrictions' => array ( ), 'id' => 1, 'icon' => 'https://render.guildwars2.com/file/4AECE5EA59CA057F4C53E1EDFE95E0E3E61DE37F/60980.png', 'details' => stdClass::__set_state(array( 'type' => 'Staff', 'damage_type' => 'Physical', 'min_power' => 146, 'max_power' => 165, 'defense' => 0, 'infusion_slots' => array ( ), 'infix_upgrade' => stdClass::__set_state(array( 'attributes' => array ( ), )), 'secondary_suffix_item_id' => '', )), )
+//
 die();
 d(__DIR__);
 var_dump(class_exists('Monolog\Handler\AmqpHandler'));
